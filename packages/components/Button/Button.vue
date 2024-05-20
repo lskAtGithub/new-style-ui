67a69c3e58ee940e8b6aaae9754844df26333a96
liushengkai <liushengkai95@gm,ail.com>
2024-05-21 00:45:15 +0800

    feat(components): Button 组件

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'NsButton'
+})
+</script>
+
+<template>
+  <button>test</button>
+</template>
---
+++ packages/components/Button/index.ts
@@ -0,0 +1,4 @@
+import Button from "./Button.vue"
+import { withInstall } from '@new-style-ui/utils'
+
+export const NsButton = withInstall(Button)
