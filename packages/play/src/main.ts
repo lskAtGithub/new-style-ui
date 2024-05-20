bb7c24e785363f257862565eb3c4d5f223a84052
liushengkai <liushengkai95@gm,ail.com>
2024-05-21 00:41:23 +0800

    feat(play)：引入 new-style-ui 库

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import NewStyle from 'new-style-ui'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(NewStyle)
+
+app.mount('#app')
